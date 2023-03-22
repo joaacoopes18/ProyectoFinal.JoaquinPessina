84125dbfcc0fe3921a2de19fdf5d2cb653bca0c1
joaacoopes18 <123560266+joaacoopes18@users.noreply.github.com>
2023-03-21 21:57:30 -0300

    Update BackUp+Pessina.sql

--- BackUp+Pessina.sql
+++ BackUp+Pessina.sql
@@ -4,6 +4,8 @@
 -- ------------------------------------------------------
 -- Server version	8.0.31
 
+-- las tablas insertadas dentro del BackUp son: familia, estado_civil, identificacion, provincia, ciudad, trabajo, clasificacion, genero, nivel_educativo
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
